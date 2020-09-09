b7ac34c14cf106871d79b6fd93c727a6827275ae
Top <50546939+hashitop@users.noreply.github.com>
2020-09-10 09:35:03 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
 variable "name" {
   description = "A name to pass into this module. (Demo only; unused)"
 }
+
+variable "some_structure" {
+  type = object({
+    name1 = string
+    name2 = string
+  )}
+}
