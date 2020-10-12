1ae3c196694a28d7a57381d235b876d1e04a6d20
Top <50546939+hashitop@users.noreply.github.com>
2020-10-12 13:40:17 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,23 @@ variable "name" {
 
 variable "some_structure" {
   description = "object({\n name1 = string\n name2 = string\n })"
-
+  default = null
+  
   type = object({
     name1 = string
     name2 = string
+    name3 = string
+    name4 = string
+    name5 = string
+    name6 = string
+    name7 = string
+    name8 = string
+    name9 = string
+    name10 = string
+    name11 = string
+    name12 = string
+    name13 = string
+    name14 = string
+    name15 = string
   })
 }
